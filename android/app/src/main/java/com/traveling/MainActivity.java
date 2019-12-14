cd1ac8022ebd3b92857ad2b8221e9874f54e5458
Miguel Mendoza <miguelemrdev@gmail.com>
2019-12-14 14:12:03 -0400

    add react-navigation

--- android/app/src/main/java/com/traveling/MainActivity.java
+++ android/app/src/main/java/com/traveling/MainActivity.java
@@ -1,15 +1,29 @@
 package com.traveling;
 
 import com.facebook.react.ReactActivity;
+/* Import Externos */
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "traveling";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
--- app/screens/WelcomeScreen/WelcomeScreen.js
+++ app/screens/WelcomeScreen/WelcomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 
 export class WelcomeScreen extends React.Component {
+  
   render() {
     <Text>Welcome Screen</Text>;
   }
